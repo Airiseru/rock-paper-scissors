ea9bdeb1009329a4d7b94a2227b51b70f28f17dc
Airiseru <dmdee2004@gmail.com>
2020-09-06 18:46:52 +0800

    Add interactivity and logic for best of 5

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+// declarations
 let playerScore = 0;
 let computerScore = 0;
+const body = document.querySelector("body");
+const scoreboard = document.querySelector(".scoreboard");
+const options = document.querySelector(".options");
+const buttons = document.querySelectorAll(".option");
+const main = document.querySelector(".main-area");
+const outputPlayerScore = document.querySelector(".playerScore");
+const outputComputerScore = document.querySelector(".computerScore");
+const outcome = document.querySelector(".outcome");
+const winner = document.createElement("h2");
+const retry = document.createElement("button");retry.classList.add("retry-button");
+
+// gives random num and diff choices for the comp 
 function computerPlay() {
     let randomNum = Math.floor(Math.random() * 3);
     if (randomNum === 0) {
@@ -13,66 +26,91 @@ function computerPlay() {
     }
 }
 
+// rock paper scissors who wins against what
 function playRound(playerSelection, computerSelection) {
     switch(playerSelection) {
         case "rock":
-            if (computerSelection === "rock") return `Draw! | Player Score: ${playerScore} | Computer Score: ${computerScore}`
-            else if (computerSelection === "paper") {
+            if (computerSelection == "rock") return `Draw!`;
+            else if (computerSelection == "paper") {
                 computerScore++;
-                return `You Lose, Paper beats Rock | Player Score: ${playerScore} | Computer Score: ${computerScore}`;
+                return `You Lose, Paper beats Rock`;
             }
             else {
                 playerScore++;
-                return `You Win! Rock beats Scissors | Player Score: ${playerScore} | Computer Score: ${computerScore}`;
+                return `You Win! Rock beats Scissors`;
             }
             break;
         case "paper":
-            if (computerSelection === "paper") return `Draw! | Player Score: ${playerScore} | Computer Score: ${computerScore}`
+            if (computerSelection === "paper") return `Draw!`
             else if (computerSelection === "scissors") {
                 computerScore++;
-                return `You Lose, Scissors beats Paper | Player Score: ${playerScore} | Computer Score: ${computerScore}`;
+                return `You Lose, Scissors beats Paper`;
             }
             else {
                 playerScore++;
-                return `You Win! Paper beats Rock | Player Score: ${playerScore} | Computer Score: ${computerScore}`;
+                return `You Win! Paper beats Rock`;
             }
             break;
         case "scissors":
-            if (computerSelection === "scissors") return `Draw! | Player Score: ${playerScore} | Computer Score: ${computerScore}`
+            if (computerSelection === "scissors") return `Draw!`
             else if (computerSelection === "rock") {
                 computerScore++;
-                return `You Lose, Rock beats Scissors | Player Score: ${playerScore} | Computer Score: ${computerScore}`;
+                return `You Lose, Rock beats Scissors`;
             }
             else {
                 playerScore++;
-                return `You Win! Scissors beats Paper | Player Score: ${playerScore} | Computer Score: ${computerScore}`;
+                return `You Win! Scissors beats Paper`;
             }
             break;
         default:
-            alert("Please enter Rock, Paper or Scissors or Please Enter Correct Spelling");
+            return "Choose An Option";
+            break;
     }
 }
 
-function game() {
-        let playerSelection;
-        for (let i = 0; i < 5; i++) {
-            playerSelection = prompt("Rock, Paper Scissors");
-            playerSelection = playerSelection.toLowerCase();
-            let computerSelection = computerPlay();
-            console.log(playRound(playerSelection, computerSelection));
-        }
+// determines who wins and the style depending on the winner
+function whoWon(playerScore, computerScore) {
+    if (playerScore == 5 && playerScore > computerScore) {
+        retry.style.backgroundColor = "#f6f7d4";
+        body.style.backgroundColor = "#28df99";
+        return "Congrats! You are the Winner!";
+    }
+    else {
+        retry.style.backgroundColor = "#e0ece4";
+        body.style.backgroundColor = "#ff4b5c";
+        return "Oops. Too Bad. You Lost.";
+    }
+}
 
-        console.log(`Final Scores: Player: ${playerScore} | Computer: ${computerScore}`);
+// once theres a winner retry button appear
+function endGame() {
+    document.getElementsByClassName("options")[0].style.display = "none";
+    winner.textContent = `${whoWon(playerScore, computerScore)}`;
+    main.appendChild(winner);
+    scoreboard.removeChild(outcome);
+    retry.textContent = "PLAY AGAIN?";
+    main.appendChild(retry);
+}
 
-        if (playerScore > computerScore) {
-            console.log("You Win!");
-        }
-        else if (computerScore > playerScore) {
-            console.log("Too bad, the computer beat you! Try again next time!");
-        }
-        else {
-            console.log("It's a Tie!");
+// what happens when you click the button
+function game(e) {
+        const playerSelection = e.target.className;
+        const computerSelection = computerPlay();
+        outcome.textContent = playRound(playerSelection, computerSelection);
+        outputPlayerScore.textContent = `${playerScore}`;
+        outputComputerScore.textContent = `${computerScore}`;
+        if (playerScore === 5 || computerScore === 5) {
+            endGame();
         }
 }
 
-// game();
+function playAgain() {
+    location.reload(true);
+}
+
+
+buttons.forEach((button) => {
+    button.addEventListener("click", game);
+});
+
+retry.addEventListener("click", playAgain);
